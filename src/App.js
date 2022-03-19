f82d93301e63d846c70a8574c34bb1033e8cceaf
lnuvy <hangeulgod@naver.com>
2022-03-19 17:08:05 +0900

    todo 틀만들어보기

--- src/App.js
+++ src/App.js
@@ -1,36 +1,35 @@
-import { Children } from "react";
-import "./App.css";
-import LifecycleEx from "./LifecycleEx";
 import React from "react";
+import logo from "./logo.svg";
+// BucketList 컴포넌트를 import 해옵니다.
+// import [컴포넌트 명] from [컴포넌트가 있는 파일경로];
+import BucketList from "./BucketList";
+import "./style.css";
 
-import { BucketList } from "./BucketList";
-
+// 클래스형 컴포넌트는 이렇게 생겼습니다!
 class App extends React.Component {
   constructor(props) {
     super(props);
-
+    // App 컴포넌트의 state를 정의해줍니다.
     this.state = {
       list: ["영화관 가기", "매일 책읽기", "수영 배우기"],
     };
   }
 
+  // 랜더 함수 안에 리액트 엘리먼트를 넣어줍니다!
   render() {
-    console.log(this.state.list);
+    // this 키워드를 통해 state에 접근할 수 있어요.
+    console.log(this.state);
+
     return (
       <div className="App">
-        <BucketList dkssudgktpdy={this.state.list} />
+        <div className="container">
+          <h1>버킷 리스트</h1>
+          <hr className="line" />
+          <BucketList list={this.state.list} />
+        </div>
       </div>
     );
   }
 }
-
-// function App() {
-
-//   return (
-//     <div className="App">
-//       <BucketList />
-//     </div>
-//   );
-// }
 
 export default App;
