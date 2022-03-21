2d1986568e629910e0ce339b7ad2bb1d7e4f7fd6
lnuvy <hangeulgod@naver.com>
2022-03-21 13:06:00 +0900

    버킷리스트 redux로 삭제하기만들기

--- src/Detail.js
+++ src/Detail.js
@@ -1,16 +1,27 @@
 import React from "react";
-import { useSelector } from "react-redux";
-import { useParams } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate, useParams } from "react-router-dom";
+import { deleteBucket } from "./redux/modules/bucket";
 
 const Detail = () => {
   const params = useParams();
   const bucket_index = params.index;
   const bucket_list = useSelector((state) => state.bucket.list);
-  console.log(params);
-  console.log(bucket_list);
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
   return (
     <div>
       <h1>{bucket_list[bucket_index]}</h1>
+      <button
+        onClick={() => {
+          console.log("삭제됨");
+          dispatch(deleteBucket(bucket_index));
+          navigate(-1);
+        }}
+      >
+        삭제하기
+      </button>
     </div>
   );
 };
--- src/redux/modules/bucket.js
+++ src/redux/modules/bucket.js
@@ -2,6 +2,7 @@
 
 // Actions
 const CREATE = "bucket/CREATE";
+const DELETE = "bucket/DELETE";
 
 const initialState = {
   list: ["영화관 가기크크루삥뽕", "매일 책읽기", "수영 배우기", "코딩때리기"],
@@ -13,12 +14,24 @@ export function createBucket(bucket) {
   return { type: CREATE, bucket };
 }
 
+export function deleteBucket(bucket_index) {
+  console.log(`버킷 인데스 ${bucket_index}`);
+  return { type: DELETE, bucket_index };
+}
+
 // Reducer
 export default function reducer(state = initialState, action = {}) {
   switch (action.type) {
     case "bucket/CREATE": {
       const new_bucket_list = [...state.list, action.bucket];
       return { list: new_bucket_list };
+    }
+    case "bucket/DELETE": {
+      const new_bucket = state.list.filter((l, i) => {
+        return parseInt(action.bucket_index) !== i;
+      });
+      console.log(new_bucket);
+      return { list: new_bucket };
     }
     default:
       return state;
